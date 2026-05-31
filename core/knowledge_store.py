672df93580ee025802273161eac0ded26dd7bc84
Feiyu-L1 <a577135620@gmail.com>
2026-05-31 14:12:56 +0800

    feat

---
+++ core/knowledge_store.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+class KnowledgeStore(ABC):
+    @abstractmethod
+    def store(self, content, doc_type, doc_id, name):
+        pass
+    @abstractmethod    
+    def retrieve(self, query):
+        pass
+
+class MockStore(KnowledgeStore):
+    def __init__(self):
+        self.documents = []     # An empty list, used to store documents
+
+    def store(self, content, doc_type, doc_id, name):
+        doc = {
+            "content": content,
+            "doc_type": doc_type,
+            "doc_id": doc_id,
+            "name": name
+        }
+        self.documents.append(doc)
+
+    def retrieve(self, query):
+        results = []
+        for doc in self.documents:
+            if query in doc["content"]:
+                results.append(doc)
+        return results
+
--- core/model_adapter.py
+++ core/model_adapter.py
@@ -1,7 +1,31 @@
 from abc import ABC, abstractmethod 
+import os
+from openai import OpenAI
 
-class ModelAdapter:
+class ModelAdapter(ABC):
 
     @abstractmethod
-    def chat(self, prompt):
+    def chat(self, prompt, system_prompt="你是一个AI助手"):
         pass
+
+class MimoAdapter(ModelAdapter):
+    def __init__(self, api_key, model):
+        self.client = OpenAI(
+            api_key = api_key,
+            base_url = "https://api.xiaomimimo.com/v1"
+        )
+        self.model = model
+    
+    def chat(self, prompt,system_prompt="你是一个AI助手"):
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": prompt}],
+            max_completion_tokens=1024,
+            temperature=1.0,
+            top_p=0.95,
+            stream=False
+        )
+        return completion.choices[0].message.content
+      
